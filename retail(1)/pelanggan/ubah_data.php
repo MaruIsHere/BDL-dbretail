9725e902e610a07147f6153e15058f3eb267f2fb
Marcell <115938178+MaruIsHere@users.noreply.github.com>
2023-06-23 10:39:39 +0800

    ubah_data.php

--- retail(1)/pelanggan/ubah_data.php
+++ retail(1)/pelanggan/ubah_data.php
@@ -1,6 +1,23 @@
 <?php
 
-$file = file_get_contents('pelanggan.json');
-$data_pelanggan = json_decode($file);
+$file = "mahasiswa.json";
+
+$anggota = file_get_contents($file);
+
+$data = json_decode($anggota, true);
+
+$nim = $_POST['nim'];
+foreach ($data as $key => $d) {
+    if ($d['nim'] === $nim) {
+        $data[$key]['nama'] = $_POST['nama'];
+        $data[$key]['no_tlp'] = $_POST['no_tlp'];
+        $data[$key]['jns_kelamin'] = $_POST['jns_kelamin'];
+        $data[$key]['alamat'] = $_POST['alamat'];
+    }
+}
+
+$jsonfile = json_encode($data, JSON_PRETTY_PRINT);
+
+$anggota = file_put_contents($file, $jsonfile);
 
 ?>
